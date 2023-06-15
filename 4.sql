6934c667e47e5c3689fecbf9c0a66df5599002a8
PavelBagaev <70954796+PavelBagaev@users.noreply.github.com>
2023-06-15 13:29:22 +0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -3,16 +3,16 @@
 --Столбцы: Фамилия покупателя | Имя покупателя | Название продукта | Количество купленных экземпляров (за все время) 
 --Упорядочить по количеству купленных экземпляров по убыванию, затем по полному имени покупателя по возрастанию
 
-SELECT person.FirstName, person.LastName, product.Name, COUNT(soh.SalesOrderID) AS 'Products bought'
-FROM Production.Product AS product
-JOIN Sales.SalesOrderDetail AS sod
-ON sod.ProductID = product.ProductID
-JOIN Sales.SalesOrderHeader AS soh
-ON soh.SalesOrderID = sod.SalesOrderID
+SELECT person.LastName AS 'First name', person.FirstName AS 'Last name', product.Name AS 'Name of the product', SUM(OrderQty) AS 'Products bought'
+FROM Sales.SalesOrderHeader AS SOH
 JOIN Sales.Customer AS customer
-ON soh.CustomerID = customer.CustomerID
+ON SOH.CustomerID = customer.CustomerID
 JOIN Person.Person AS person
 ON customer.PersonID = person.BusinessEntityID
-GROUP BY person.FirstName, person.LastName, product.Name
-HAVING COUNT(soh.SalesOrderID) > 15
-ORDER BY COUNT(soh.SalesOrderID) DESC;
+JOIN Sales.SalesOrderDetail SOD
+ON SOH.SalesOrderID = SOD.SalesOrderID
+JOIN Production.Product AS product
+ON SOD.ProductID = product.ProductID
+GROUP BY person.LastName, person.FirstName, product.Name
+HAVING SUM(OrderQty) > 15
+ORDER BY [Products bought] DESC, person.LastName, person.FirstName;
